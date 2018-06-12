a63a343773abbe174fcab96488583209c0184483
Anton <a.kritikos1997@gmail.com>
2018-06-12 14:40:31 +0200

    Update URL en Blipps

--- HU_Blipp_Back/javascript/main.js
+++ HU_Blipp_Back/javascript/main.js
@@ -18,10 +18,14 @@ scene.onCreate = function() {
         closeButton1 = scene.addSprite().setTexture('close_button.png').setScale(60, 60, 0).setTranslation(-317, 240.5,120).setHidden(true),
       imgButton2 = scene.addSprite().setTexture('button_img_2.png').setScale(180, 180, 0).setTranslation(157.5, 80, 1),
 		buttonText2 = scene.addSprite().setTexture('button_text_2.jpg').setScale(340, 60, 0).setTranslation( -75, 80, 0),
+        textLayer2 = scene.addSprite().setTexture('text_layer_2.jpg').setScale(369, 537, 0).setTranslation( -150, -243, 101).setHidden(true),
+          videoButton2 = scene.addSprite().setTexture('button_video_1.jpg').setScale(330, 55, 0).setTranslation(-150, -395, 111).setHidden(true),
+          websiteButton2 = scene.addSprite().setTexture('button_website_1.jpg').setScale(330, 55, 0).setTranslation(-150, -460, 111).setHidden(true),
+        closeButton2 = scene.addSprite().setTexture('close_button.png').setScale(60, 60, 0).setTranslation(-317, 51,120).setHidden(true),
       imgButton3 = scene.addSprite().setTexture('button_img_3.png').setScale(180, 180, 0).setTranslation(157.5, -107.5, 1),
 		buttonText3 = scene.addSprite().setTexture('button_text_3.jpg').setScale(340, 60, 0).setTranslation( -75, -107.5, 0),
 	  infoButton = scene.addSprite().setTexture('more_info.jpg').setScale(315, 120, 0).setTranslation((mW * 0.35), (mH - (mH * 1.45)), 0),
-	  webURL = "http://19416.hosts.ma-cloud.nl/";
+	  webURL = "http://19578.hosts.ma-cloud.nl/hu/";
 
 
   textLayer1.onCreate = function(time) {
@@ -51,8 +55,54 @@ scene.onCreate = function() {
     this.animationStartO = 0;
     this.animationEndO = 0;
   }
+  
+  textLayer2.onCreate = function(time) {
+    this.animationStartT = this.getTranslation();
+    this.animationEndT = this.getTranslation();
+    this.animationStartO = 0;
+    this.animationEndO = 0;
+  }
+
+  closeButton2.onCreate = function(time) {
+    this.animationStartT = this.getTranslation();
+    this.animationEndT = this.getTranslation();
+    this.animationStartO = 0;
+    this.animationEndO = 0;
+  }
+
+  videoButton2.onCreate = function(time) {
+    this.animationStartT = this.getTranslation();
+    this.animationEndT = this.getTranslation();
+    this.animationStartO = 0;
+    this.animationEndO = 0;
+  }
+
+  websiteButton2.onCreate = function(time) {
+    this.animationStartT = this.getTranslation();
+    this.animationEndT = this.getTranslation();
+    this.animationStartO = 0;
+    this.animationEndO = 0;
+  }
 
   imgButton1.onCreate = function(time) {
+    this.animationStartT = this.getTranslation();
+    this.animationEndT = this.getTranslation();
+    this.animationStartO = 1;
+    this.animationEndO = 1;
+    this.animationStartTime = -1;
+    this.animationDuration = 500;
+  }
+  
+  imgButton2.onCreate = function(time) {
+    this.animationStartT = this.getTranslation();
+    this.animationEndT = this.getTranslation();
+    this.animationStartO = 1;
+    this.animationEndO = 1;
+    this.animationStartTime = -1;
+    this.animationDuration = 500;
+  }
+  
+  imgButton3.onCreate = function(time) {
     this.animationStartT = this.getTranslation();
     this.animationEndT = this.getTranslation();
     this.animationStartO = 1;
@@ -92,6 +142,7 @@ scene.onCreate = function() {
   textLayer1.onUpdate = function(time) {
     animateObj(this, time);
   }
+  
 
   closeButton1.onUpdate = function(time) {
     animateObj(this, time);
@@ -104,8 +155,33 @@ scene.onCreate = function() {
   websiteButton1.onUpdate = function(time) {
     animateObj(this, time);
   }
+  
+  textLayer2.onUpdate = function(time) {
+    animateObj(this, time);
+  }
+  
+
+  closeButton2.onUpdate = function(time) {
+    animateObj(this, time);
+  }
+
+  videoButton2.onUpdate = function(time) {
+    animateObj(this, time);
+  }
+
+  websiteButton2.onUpdate = function(time) {
+    animateObj(this, time);
+  }
 
   imgButton1.onUpdate = function(time) {
+    animateObj(this, time);
+  }
+  
+  imgButton2.onUpdate = function(time) {
+    animateObj(this, time);
+  }
+  
+  imgButton3.onUpdate = function(time) {
     animateObj(this, time);
   }
   
@@ -122,6 +198,22 @@ scene.onCreate = function() {
   }
 
   imgButton1.on('touchEnd', function() {
+	  if (!textLayer2.getHidden()) {
+		hide([textLayer2,closeButton2,videoButton2,websiteButton2]);
+		imgButton2.animationStartT = [127.5, 50, 121];
+		imgButton2.animationEndT = [157.5, 80, 1];
+		imgButton2.animationStartTime = -1;
+		imgButton2.animationDuration = 500;
+		// button text
+		buttonText2.animationStartT = [-120, 50, 120];
+		buttonText2.animationEndT = [-75, 80, 0];
+		buttonText2.animationStartTime = -1;
+		buttonText2.animationDuration = 500;
+		var timeout = setTimeout(function(){
+		imgButton2.setTranslationZ(1);
+		buttonText2.setTranslationZ(0);
+	  },500);
+	}
     if (textLayer1.getHidden()) {
       show([textLayer1,closeButton1,videoButton1,websiteButton1]);
       this.animationStartT = [157.5, 270, 1];
@@ -143,6 +235,22 @@ scene.onCreate = function() {
   });
   
   buttonText1.on('touchEnd',function() {
+	  if (!textLayer2.getHidden()) {
+		hide([textLayer2,closeButton2,videoButton2,websiteButton2]);
+		imgButton2.animationStartT = [127.5, 50, 121];
+		imgButton2.animationEndT = [157.5, 80, 1];
+		imgButton2.animationStartTime = -1;
+		imgButton2.animationDuration = 500;
+		// button text
+		buttonText2.animationStartT = [-120, 50, 120];
+		buttonText2.animationEndT = [-75, 80, 0];
+		buttonText2.animationStartTime = -1;
+		buttonText2.animationDuration = 500;
+		var timeout = setTimeout(function(){
+		imgButton2.setTranslationZ(1);
+		buttonText2.setTranslationZ(0);
+	  },500);
+	}
 	  if (textLayer1.getHidden()) {
       show([textLayer1,closeButton1,videoButton1,websiteButton1]);
       imgButton1.animationStartT = [157.5, 270, 1];
@@ -187,7 +295,90 @@ scene.onCreate = function() {
   });
   
   imgButton2.on('touchEnd', function() {
-    blipp.openURL(webURL);
+	if (!textLayer1.getHidden()) {
+		hide([textLayer1,closeButton1,videoButton1,websiteButton1]);
+		imgButton1.animationStartT = [127.5, 240, 121];
+		imgButton1.animationEndT = [157.5, 270, 1];
+		imgButton1.animationStartTime = -1;
+		imgButton1.animationDuration = 500;
+		// button text
+		buttonText1.animationStartT = [-120, 240, 120];
+		buttonText1.animationEndT = [-75, 270, 0];
+		buttonText1.animationStartTime = -1;
+		buttonText1.animationDuration = 500;
+		var timeout = setTimeout(function(){
+		imgButton1.setTranslationZ(1);
+		buttonText1.setTranslationZ(0);
+	  },500);
+	}
+	if (textLayer2.getHidden()) {
+      show([textLayer2,closeButton2,videoButton2,websiteButton2]);
+      imgButton2.animationStartT = [157.5, 80, 1];
+      imgButton2.animationEndT = [127.5, 50, 121];
+      imgButton2.animationStartTime = -1;
+      imgButton2.animationDuration = 500;
+	  // button text
+	  buttonText2.animationStartT = [-75, 80, 0];
+      buttonText2.animationEndT = [-120, 50, 120];
+      buttonText2.animationStartTime = -1;
+      buttonText2.animationDuration = 500;
+	  var timeout = setTimeout(function(){
+		imgButton2.setTranslationZ(121);
+		buttonText2.setTranslationZ(120);
+	  },500);
+    }
+  });
+  
+  buttonText2.on('touchEnd', function() {
+	if (!textLayer1.getHidden()) {
+		hide([textLayer1,closeButton1,videoButton1,websiteButton1]);
+		imgButton1.animationStartT = [127.5, 240, 121];
+		imgButton1.animationEndT = [157.5, 270, 1];
+		imgButton1.animationStartTime = -1;
+		imgButton1.animationDuration = 500;
+		// button text
+		buttonText1.animationStartT = [-120, 240, 120];
+		buttonText1.animationEndT = [-75, 270, 0];
+		buttonText1.animationStartTime = -1;
+		buttonText1.animationDuration = 500;
+		var timeout = setTimeout(function(){
+		imgButton1.setTranslationZ(1);
+		buttonText1.setTranslationZ(0);
+	  },500);
+	}
+	if (textLayer2.getHidden()) {
+      show([textLayer2,closeButton2,videoButton2,websiteButton2]);
+      imgButton2.animationStartT = [157.5, 80, 1];
+      imgButton2.animationEndT = [127.5, 50, 121];
+      imgButton2.animationStartTime = -1;
+      imgButton2.animationDuration = 500;
+	  // button text
+	  buttonText2.animationStartT = [-75, 80, 0];
+      buttonText2.animationEndT = [-120, 50, 120];
+      buttonText2.animationStartTime = -1;
+      buttonText2.animationDuration = 500;
+	  var timeout = setTimeout(function(){
+		imgButton2.setTranslationZ(121);
+		buttonText2.setTranslationZ(120);
+	  },500);
+    }
+  });
+  
+  closeButton2.on('touchEnd', function() {
+    hide([textLayer2,closeButton2,videoButton2,websiteButton2]);
+		imgButton2.animationStartT = [127.5, 50, 121];
+		imgButton2.animationEndT = [157.5, 80, 1];
+		imgButton2.animationStartTime = -1;
+		imgButton2.animationDuration = 500;
+		// button text
+		buttonText2.animationStartT = [-120, 50, 120];
+		buttonText2.animationEndT = [-75, 80, 0];
+		buttonText2.animationStartTime = -1;
+		buttonText2.animationDuration = 500;
+		var timeout = setTimeout(function(){
+		imgButton2.setTranslationZ(1);
+		buttonText2.setTranslationZ(0);
+	  },500);
   });
   
   imgButton3.on('touchEnd', function() {
